722dde12dc1e94a819708dba79c2d48597a1738e
michael-a-jones <96099638+michael-a-jones@users.noreply.github.com>
2022-07-06 11:34:41 +1000

    Create estimator_mwe.py
    a minimum working (hopefully) example of an estimator

---
+++ estimator_mwe.py
@@ -0,0 +1,38 @@
+from qiskit import QuantumCircuit
+from qiskit.circuit.parameter import Parameter
+from qcchem.backends.interface import get_backendwrapper
+
+def evaluate_pauli(pauli,results):
+    E=0
+    for bitstring,weight in results.items():
+        sign=1
+        for P,B in zip(pauli,bitstring):
+            if B=='1' and not P=='I': sign=-sign
+        E+=sign*weight
+    return E/sum(results.values())
+
+backend=get_backendwrapper('sim0')
+theta=0
+
+theta_=Parameter('t')
+circ=QuantumCircuit(2,2)
+circ.x(0)
+circ.h(0)
+circ.cx(0,1)
+circ.ry(theta_,0)
+circ.ry(theta_,1)
+circ.cx(0,1)
+circ.h(0)
+
+Ham={'XX':1,'YY':1,'ZZ':1}
+bases=tuple(PauliBasis(b) for b in Ham)
+
+counts=backend.run(circ.assign_parameters({theta_:theta}),bases)
+
+E=0
+for pauli in Ham:
+    for c in counts:
+        if c.contains(pauli):
+            E+=evaluate_pauli(pauli,c.results)
+            continue
+print(E)
